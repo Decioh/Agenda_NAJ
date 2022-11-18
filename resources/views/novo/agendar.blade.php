669cd7e4e6b42016c11c00b8475228bdcb24096d
decioh <deciocarretta@hotmail.com>
2022-11-18 16:59:35 -0300

    styling

--- resources/views/novo/agendar.blade.php
+++ resources/views/novo/agendar.blade.php
@@ -9,16 +9,31 @@
 
     
     @foreach($events as $event)
-
-            <div id="cards-container">
+        @if($loop->first)                                                   <!-- Se for o primeiro loop, já criamos o card com o dia-->
+            @php $day = date('d/m', strtotime($event -> start))@endphp      <!-- Igualando o primeiro dia com o dia atual, para comparar nos próximos loops-->
+            <div id="cards-container">                                      <!-- Container para os cards-->
             <div class="row d-flex justify-content-center">
                 <div class = "card col-md-4">
                     <div class="card-body">
-                        <h5 class="card-date">{{$event -> dia}}<br>dia {{date('d/m', strtotime($event -> start))}}</h5>
-                        <p class="datas"> de {{date('H:i', strtotime($event -> start))}} <br> até {{date('H:i', strtotime($event -> end))}}</p>
-                        <p class="vagas">{{$event -> vag_h}} vagas</p>
-            
-                        <a href="/novo/{{ $event -> id }}" class="btn btn-secondary"> {{date('H:i', strtotime($event -> start))}} </a>
+                        <h5 class="card-date">{{$event -> dia}}<br>dia {{date('d/m', strtotime($event -> start))}}</h5><!-- Imprimindo o dia e mês-->
+        @endif
+            @if($day != date('d/m', strtotime($event -> start)))    <!-- Se for um novo dia, criaremos outro card com o próximo dia-->
+            <div id="cards-container">
+            <div class="row d-flex justify-content-center">
+                <div class = "card col-md-4">
+                        <div class="card-body">
+                            <h5 class="card-date">{{$event -> dia}}<br>dia {{date('d/m', strtotime($event -> start))}}</h5>
+                            <p class="datas"> de {{date('H:i', strtotime($event -> start))}} <br> até {{date('H:i', strtotime($event -> end))}} <!-- Imprimindo o horario de atendimento-->
+            @else
+                <p class="datas"> de {{date('H:i', strtotime($event -> start))}} <br> até {{date('H:i', strtotime($event -> end))}} <br> <!-- Se for o mesmo dia... -->
+            @if (($event -> assistido) == "Horário vago")                           <!-- Se o horario estiver vago, habilita link para cadastrar assistido -->
+                <a href="/novo/{{ $event -> id }}" class="btn btn-success"> Vago </a></p>
+            @else                                                                   <!-- Se o horario não estiver vago, habilita link para ver informações do agendamento -->
+                <a href="/{{ $event -> id }}" class="btn btn-secondary"> <ion-icon name="information-circle-outline"> INFO </ion-icon> </a></p>
+            @endif
+            @endif
+                    </div>
+                        @php $day = date('d/m', strtotime($event -> start))@endphp
                     </div>
                 </div>
             </div>
@@ -27,7 +42,7 @@
     @if(count($events) == 0)
         <p>Não há agendamentos disponíveis</p>
     @endif
-    </div>
+    
 
 
 @endsection  
